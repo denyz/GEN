e62bc0f01fd850dab44ebd83272cbd458301f160
denyz <denyz@126.com>
2025-05-05 11:34:38 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,3 @@
 python src/gnnradarobjectdetection/train.py --data data/datasets/nuscenes/processed/ --result data/results/ --config configurations/configuration_nuscenes.yml
 #python src/gnnradarobjectdetection/train.py --data data/datasets/radarscenes/processed/ --result data/results/ --config configurations/configuration_radarscenes.yml
+#python src/gnnradarobjectdetection/train.py --data data/datasets/radarscenes/processed/ --result data/results/ --config configurations/configuration_VOD.yml
